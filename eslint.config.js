52a1f85b2309b45385286c40cf083041612b3519
Niels de Boer <niels.deboer@athom.nl>
2024-08-05 16:09:35 +0200

    feat(dsmr): add prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: [
+      'dist/**/*',
+      'eslint.config.js',
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      ...prettier.rules,
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/require-await': 'off',
+      'no-unused-vars': 'off',
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ],
+      // Allow floating promises in tests (source: https://github.com/nodejs/node/issues/51292#issuecomment-2241761056)
+      "@typescript-eslint/no-floating-promises": [
+        "error",
+        {
+            "allowForKnownSafeCalls": [
+                {
+                    "from": "package",
+                    "name": [ "it", "describe" ],
+                    "package": "node:test"
+                }
+            ]
+        }
+      ],
+    }
+  },
+);
