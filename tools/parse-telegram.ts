55ff9a459867cbf2920c998d655469419a2934b3
Niels de Boer <niels.deboer@athom.nl>
2024-12-20 12:26:06 +0100

    Update parse-telegram script to parse from a hex-encoded file

--- tools/parse-telegram.ts
+++ tools/parse-telegram.ts
@@ -2,7 +2,8 @@
 /** This script is used to parse a DSMR telegram from a file. */
 import { promises as fs } from 'node:fs';
 import path from 'node:path';
-import { DSMR } from '../src/index.js';
+import { PassThrough } from 'node:stream';
+import { DSMR, DSMRError } from '../src/index.js';
 
 const filePath = process.argv[2];
 
@@ -11,16 +12,43 @@ if (filePath === undefined) {
   process.exit(1);
 }
 
+const decryptionKey = process.argv[3];
+
 const resolvedPath = path.resolve(process.cwd(), filePath);
 
-const file = await fs.readFile(resolvedPath, 'utf-8');
+let file = await fs.readFile(resolvedPath);
+const fileString = file.toString('utf-8');
 
-const parsed = DSMR.parse({ telegram: file, newLineChars: '\n' });
+const isHexFile = /^[0-9a-fA-F\s]+$/.test(fileString);
 
-console.log(JSON.stringify(parsed, null, 2));
-
-if (parsed.crc?.valid === false) {
-  console.log();
-  console.error('CRC validation failed');
-  process.exit(1);
+if (isHexFile) {
+  file = Buffer.from(fileString.replace(/\s/g, ''), 'hex');
 }
+
+const passthrough = new PassThrough();
+
+// Stream is not necessary for this script, but it allows to detect encryption in the frame.
+DSMR.createStreamParser({
+  stream: passthrough,
+  newLineChars: isHexFile ? '\r\n' : '\n', // Use CRLF for hex files as thats what used by meters
+  decryptionKey,
+  detectEncryption: true,
+  fullFrameRequiredWithinMs: 1,
+  callback: (error, result) => {
+    if (error instanceof DSMRError) {
+      console.error(error.message);
+      console.log(error.rawTelegram?.toString('hex'));
+    } else if (error) {
+      console.error(error);
+    } else if (result?.crc?.valid === false) {
+      console.log('CRC validation failed');
+      console.log(result);
+    } else {
+      console.log(result);
+    }
+
+    process.exit(0);
+  },
+});
+
+passthrough.write(file);
